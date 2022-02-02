ebda8ebe73c846da3bde65d43c1ba8c42e98e39c
antheiz <salupacode@gmail.com>
2022-02-03 08:07:43 +0900

    day-2

---
+++ day-2/sintaks.php
@@ -0,0 +1,42 @@
+<?php
+// 3. Sintaks PHP
+
+/* 
+    1. PHP di dalam HTML
+    2. HTML di dalam PHP
+*/
+
+?>
+
+<!-- 1. PHP di dalam HTML -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h2>Halo Bro <?php echo "Theis Andatu" ?></h2>
+</body>
+</html>
+
+
+<!-- 2. HTML di dalam PHP -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Halo Broo Theis</h1>";
+    ?>
+</body>
+</html>
---
+++ day-2/standart_output.php
@@ -0,0 +1,10 @@
+<?php
+// 2. Standard Output
+
+echo "Theis Andatu"; 
+print "Theis Andatu"; 
+
+print_r("Theis Andatu");
+var_dump("Theis Andatu");
+
+?>
